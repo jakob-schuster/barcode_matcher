d91e705c77afd87e36fb5b0d4bb0bfb87566d09b
jakob <jakobschuster0@gmail.com>
2021-12-16 12:27:27 +1100

    changed some names

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -O2 -o match_cell_barcode src/ssw/ssw_cpp.cpp src/ssw/ssw.c src/match_cell_barcode.cpp src/kseq.h src/edit_dist.cpp -lz
+g++ -std=c++11 -O2 -o barcode_matcher src/ssw/ssw_cpp.cpp src/ssw/ssw.c src/barcode_matcher.cpp src/kseq.h src/edit_dist.cpp -lz
